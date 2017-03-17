c339082463e4b17b89b4588d0612add2e5c0bfdf
ZDzhao <13716575282@163.com>
2017-03-17 18:00:21 +0800

    Update util.py
    add functions

--- util.py
+++ util.py
@@ -1,5 +1,13 @@
+import binascii
+import base64
+
 def str2base64(data):
-    d = data.replace(' ', '')
-    s = binascii.unhexlify(d)
-    r = base64.b64encode(s)
-    print(r)
+    d = data.replace(' ', '')#去空格
+    s = binascii.unhexlify(d)#16进制转字符串
+    r = base64.b64encode(s)#base64编码
+    return r
+
+def b64tostr(data):
+    r = base64.b64decode(data)#base64解码
+    s = binascii.hexlify(r)#字符串转16进制数
+    return bytes.decode(s)#bytes转string
